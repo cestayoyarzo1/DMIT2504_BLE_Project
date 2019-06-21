dfdf78137131638eb8e7b7bdc2e8ae1e58a2e2c2
Carlos Estay <39070811+cestayoyarzo1@users.noreply.github.com>
2019-06-21 15:28:20 -0600

    fixed permissions problem. Scan works

--- FinalProject/app/src/main/java/dmit2504/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/dmit2504/finalproject/MainActivity.java
@@ -1,20 +1,30 @@
 package dmit2504.finalproject;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattServer;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,17 +40,23 @@ public class MainActivity extends AppCompatActivity {
     private BluetoothGattCharacteristic customCharacteristic;
 
     // Stops scanning after 10 seconds.
-    private static final long SCAN_PERIOD = 10000;
+    private static final long SCAN_PERIOD = 20000;
 
 
     //Locally-defined integer (which must be greater than 0) that the system passes back to you
     // in your onActivityResult(int, int, android.content.Intent) implementation as the requestCode parameter.
     static final int REQUEST_ENABLE_BT = 3;
 
+
+    TextView statusTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        statusTextView = findViewById(R.id.activity_main_status_text);
+
         //setContentView(R.layout.activity_device_scan);
         handler = new Handler();
         //this.bleTopText = findViewById(R.id.bleTopText);
@@ -65,17 +81,82 @@ public class MainActivity extends AppCompatActivity {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Please grant location access so this app can detect peripherals.", Toast.LENGTH_SHORT).show();
+//                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//                builder.setTitle("This app needs location access");
+//                builder.setMessage("Please grant location access so this app can detect peripherals.");
+//                builder.setPositiveButton(android.R.string.ok, null);
+//                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+//                    @RequiresApi(api = Build.VERSION_CODES.M)
+//                    @Override
+//                    public void onDismiss(DialogInterface dialog) {
+//                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, Integer.parseInt(Manifest.permission.ACCESS_COARSE_LOCATION));
+//                    }
+//                });
+//                builder.show();
+            }
+        }
+
+
+
+        scanLeDevice(true);
+
     }
 
+    public void onScanPress(View view)
+    {
+        Toast.makeText(this, "You have pressed scan", Toast.LENGTH_SHORT).show();
+        scanLeDevice(true);
+    }
 
+    private void scanLeDevice(final boolean enable) {
+        final BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+        //        leAdapter.clear();
+        //        leAdapter.notifyDataSetChanged();
+        if (enable) {
+            statusTextView.setText("Scanning...");
+            // Stops scanning after a pre-defined scan period.
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    bluetoothLeScanner.stopScan(leScanCallback);
+                    statusTextView.setText("Scan stopped");
+                }
+            }, SCAN_PERIOD);
+            mScanning = true;
+            bluetoothLeScanner.startScan(leScanCallback);
+        } else {
+            mScanning = false;
+            bluetoothLeScanner.stopScan(leScanCallback);
+        }
+    }
 
     //le Callback
-    private ScanCallback leScanCallback = new ScanCallback() {
+
+    private final ScanCallback leScanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
+            
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            System.out.println("BLE// onBatchScanResults");
+            for (ScanResult sr : results) {
+                Log.i("ScanResult - Results", sr.toString());
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            System.out.println("BLE// onScanFailed");
+            Log.e("Scan Failed", "Error Code: " + errorCode);
         }
     };
 
-    
 }
