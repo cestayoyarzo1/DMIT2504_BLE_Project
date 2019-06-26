58bea61afaac962388cf69f3f17a99835dc47904
Carlos Estay <39070811+cestayoyarzo1@users.noreply.github.com>
2019-06-26 16:14:41 -0600

    detecting devices now

---
+++ FinalProject/app/src/main/java/dmit2504/finalproject/LeDeviceData.java
@@ -0,0 +1,8 @@
+package dmit2504.finalproject;
+
+public class LeDeviceData {
+    public String Name;
+    public String Address;
+    public String Power;
+    public String Data;
+}
--- FinalProject/app/src/main/java/dmit2504/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/dmit2504/finalproject/MainActivity.java
@@ -1,6 +1,7 @@
 package dmit2504.finalproject;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
@@ -14,11 +15,15 @@ import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -50,12 +55,17 @@ public class MainActivity extends AppCompatActivity {
 
     TextView statusTextView;
 
+    public static final String APP_NAME = "Robot Control by Carlos Estay";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         statusTextView = findViewById(R.id.activity_main_status_text);
+        statusTextView.setText("");
+
+        getSupportActionBar().setTitle(APP_NAME);
+
 
         //setContentView(R.layout.activity_device_scan);
         handler = new Handler();
@@ -102,24 +112,55 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        scanLeDevice(true);
+        //scanLeDevice(true);
 
     }
+
+    //Main Menu Loading
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Create an instance of the Menu inflater
+        MenuInflater inflater = getMenuInflater();
+        // Inflate the menu
+        inflater.inflate(R.menu.main_menu, menu);
+        // Return true if the menu inflated OK
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Get the id of the selected menu item
+        switch (item.getItemId()) {
+            case R.id.menu_control:
+                // create an intent to start the ViewRemoteDataActivity
+                Intent intent = new Intent(this, RemoteControlActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //Scanning
     public void onScanPress(View view)
     {
-        Toast.makeText(this, "You have pressed scan", Toast.LENGTH_SHORT).show();
         scanLeDevice(true);
-        Intent intent = new Intent(this, RemoteControlActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, RemoteControlActivity.class);
+//        startActivity(intent);
     }
 
+    @TargetApi(21)
     private void scanLeDevice(final boolean enable) {
+
         final BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
         //        leAdapter.clear();
         //        leAdapter.notifyDataSetChanged();
         if (enable) {
             statusTextView.setText("Scanning...");
             // Stops scanning after a pre-defined scan period.
+
             handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
@@ -138,16 +179,18 @@ public class MainActivity extends AppCompatActivity {
 
     //le Callback
 
-    private final ScanCallback leScanCallback = new ScanCallback() {
+    private ScanCallback leScanCallback = new ScanCallback() {
+
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
-
+            Toast.makeText(getApplicationContext(), "Device Found", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onBatchScanResults(List<ScanResult> results) {
             System.out.println("BLE// onBatchScanResults");
+            Toast.makeText(getApplicationContext(), "Devices Found", Toast.LENGTH_SHORT).show();
             for (ScanResult sr : results) {
                 Log.i("ScanResult - Results", sr.toString());
             }
@@ -155,6 +198,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onScanFailed(int errorCode) {
+            Toast.makeText(getApplicationContext(), "Scan Failed", Toast.LENGTH_SHORT).show();
             System.out.println("BLE// onScanFailed");
             Log.e("Scan Failed", "Error Code: " + errorCode);
         }
